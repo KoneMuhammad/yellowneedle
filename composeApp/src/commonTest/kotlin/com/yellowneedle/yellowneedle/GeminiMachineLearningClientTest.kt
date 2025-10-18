4f890a7ecfe110c6192a06901785878cb2102ae3
muhamamdkone <muhammadkone@tuta.com>
2025-10-17 21:15:50 -0400

    test(data): add service response  test

--- composeApp/src/commonTest/kotlin/com/yellowneedle/yellowneedle/GeminiMachineLearningClientTest.kt
+++ composeApp/src/commonTest/kotlin/com/yellowneedle/yellowneedle/GeminiMachineLearningClientTest.kt
@@ -1,17 +1,25 @@
 package com.yellowneedle.yellowneedle
 
 import com.yellowneedle.yellowneedle.data.GeminiMachineLearningClient
+import kotlinx.coroutines.delay
 import kotlin.test.Test
+
+
 object FakeGeminiMachineLearningClient {
 
-    fun
+    suspend fun simulateMachineLearningServiceRequest() : String {
+        delay(1000)
+
+       return "Resposne from Service Success"
+    }
 //how to test suspend functions
     //how to make test from inside out
 }
 class GeminiMachineLearningClientTest {
 
     @Test
-    fun GeminiMachineLearningClient_GetResponse_ResponseSuccessfull(){
+    fun GeminiMachineLearningClient_GetResponse_ResponseSuccessfull() = runTest
+    {
 
     }
 }
