5e7a4d92175734645c9b5e907ab110d1496b961f
muhamamdkone <muhammadkone@tuta.com>
2025-10-25 18:46:37 -0400

    feat(ui): add search screen composables

--- composeApp/src/androidMain/kotlin/com/yellowneedle/yellowneedle/ui/viewmodel/searchScreen.kt
+++ composeApp/src/androidMain/kotlin/com/yellowneedle/yellowneedle/ui/viewmodel/searchScreen.kt
@@ -1,4 +1,31 @@
 package com.yellowneedle.yellowneedle.ui.viewmodel
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.yellowneedle.yellowneedle.data.dto.ArxivFeed
+
+// incorporate different view  ie 4 windows for quicker search
+//have that one the default
+
+@Composable
+fun SearchScreen() {
+
+}
+
+@Composable
+fun ArxivFeedList(arxivfeed: ()-> ArrayList<ArxivFeed>) {
+    LazyColumn {
+        items(arxivfeed()){
+            arxivfeed ->
+            TitleText {arxivfeed}
+        }
+    }
+}
+
+@Composable
+fun TitleText(arxivFeed: () -> ArxivFeed) {
+    Text(text = arxivFeed().title)
+}
 
